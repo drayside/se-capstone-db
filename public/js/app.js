b9d11e38693d78aaf76f2fade02de49380a6a319
Dominic Liu <onlylakehouse@gmail.com>
2016-11-14 12:41:31 -0500

    add tags, more styling

--- public/js/app.js
+++ public/js/app.js
@@ -30,15 +30,24 @@ app.controller("overviewController", ["$scope", "$http", function($scope, $http)
       project.interested_students_count = project.interested_students.length || 0;
       $scope.projects.push(project);
     });
+    $scope.tags = _.uniq(_.flatten(_.pluck($scope.projects, "tags")));
   });
 
   $scope.propertyName = 'id';
   $scope.reverse = false;
 
   $scope.sortBy = function(propertyName) {
-    $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
+    $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : (propertyName === "interested_students_count");
     $scope.propertyName = propertyName;
   };
+
+  $scope.tag = null;
+  $scope.setTag = function(tag) {
+    $scope.tag = tag;
+  };
+  $scope.tagFilter = function(project, index) {
+    return !$scope.tag || _.indexOf(project.tags, $scope.tag) >= 0;
+  }
 }]);
 
 app.controller("projectController", ["$scope", "$stateParams", function($scope, $stateParams) {
