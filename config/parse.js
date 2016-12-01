4523e00f7d6f8d48ecbc37949a84e3f2f5e47e90
Derek Rayside <drayside@uwaterloo.ca>
2016-12-01 07:58:45 -0500

    handling initial implicit sublist

--- config/parse.js
+++ config/parse.js
@@ -31,10 +31,10 @@ function firstMatch(a, r, offset) {
 
 function levelRegex(level) {
     switch (level) {
-        case 1 : return /^#/;
-        case 2 : return /^##/;
-        case 3 : return /^###/;
-        case 4 : return /^####/;
+        case 1 : return /^# /;
+        case 2 : return /^## /;
+        case 3 : return /^### /;
+        case 4 : return /^#### /;
         default: throw ("unknown level" + level);
     }
 }
@@ -73,7 +73,7 @@ function extractList(a, level, header, emptyStructure, expectDeep) {
     if (start < 0) {
         // didn't find the header
         // return an empty list
-        //console.log("header not found: " + header);
+        console.log("list header not found: " + header);
         return [];
     } else {
         // found the header
@@ -83,7 +83,7 @@ function extractList(a, level, header, emptyStructure, expectDeep) {
         for (var i = 0; i < level; i++) {
             end = firstMatch(a, levelRegex(level-i), start+1);
             if (end > 0) {
-                //console.log("found end: " + start + " " + end);
+                //console.log("found end: " + start + " " + end + " " + level + " " + i);
                 break;
             }
         }
@@ -92,38 +92,91 @@ function extractList(a, level, header, emptyStructure, expectDeep) {
             end = a.length
             //console.log("setting end to end of file: " + start + " " + end);
         }
+
+
         // is this a deep list?
         // extract the named sublists (each of which is itself flat) ...
         var substart = start;
+        var substarts = [];
         var deep = {};
         //console.log("--");
         do {
-            //console.log("substart: " + substart);
             var sublevel = level + 1;
             substart = firstMatch(a, levelRegex(sublevel), substart+1);
+            //console.log("substart: " + substart + " " + end);
             if (substart > 0 && substart <= end) {
-                // there is a subheading
+                // there is a subheading within bounds
+                substarts.push(substart);
                 var subhead = a[substart];
+                //console.log("recurse: " + subhead);
                 var sublist = extractFlatList(a, sublevel, subhead);
+                //console.log("returned from " + subhead);
                 subhead = subhead.replace(levelRegex(sublevel), "").trim();
                 deep[subhead] = sublist;
             } else {
                 substart = -1;
             }
         } while (substart > 0 && substart <= end);
+       
+        // if we expected a deep list, we are going to return that structure
+        if (expectDeep) {
+            // did we find a deep list?
+            if (Object.keys(deep).length > 0) {
+                // we found a deep list
+                // did it have an implicit initial sublist?
+                if (start != substarts[0]) {
+                    console.log("implicit initial sublist: " + start + " " + substarts[0]);
+                    var implicit = extractFlatList(a, level, header);
+                    deep["implicit"] = implicit;
+                    console.log("implicit: " + implicit);
+                    return deep;
+                } else {
+                    // the normal case: expected a deep list and found it
+                    return deep;
+                }
+            } else {
+                // we did not find a deep list: make the structure
+                // inject a sublist heading
+                deep["sublist"] = extractFlatList(a, level, header);
+                return deep;
+            }
+        }
+        // else we did not expect a deep list: keep going flat
+        
+       
+        // might need to tighten the end
+        if (substarts.length > 0) {
+            // there are sublists, but we aren't expecting them
+            // so tighten the end to the first sublist
+            end = substarts[0];
+        }
+
+/*
         // was it a deep list?
         if (Object.keys(deep).length > 0) {
             if (!expectDeep) {
                 console.log("found a deep list but was not expecting one: " + subhead);
+            } else {
+                // expected a deep list and found one
+                // but was there an implicit, unnamed sublist at top?
+                if (start != substarts[0]) {
+                    console.log("implicit initial sublist: " + start + " " + substarts[0]);
+                    var foo = extractFlatList(a, level, header);
+                    console.log("foo: " + foo);
+                } else {
+                    console.log("no implicit initial sublist");
+                }
             }
             //console.log(deep);
             return deep;
         } else if (expectDeep) {
             // we did not observe subheadings, but a deep list is expected
+            // so inject a sublist heading in order to return the proper structure
             deep["sublist"] = extractFlatList(a, level, header);
             //console.log(deep);
             return deep;
         }
+*/
 
         // extract each datum
         var list = emptyStructure;
@@ -174,8 +227,31 @@ var parse = function (fileName) {
     result[fileName]["blurb"] = blurb;
 
 
-    extractDeepList(fileContent, 2, /^##\s*Interested\s+Students/);
+    // extract students
+    console.log(fileName + " students  ------------------------");
+    var students = extractDeepList(fileContent, 2, /^##\s*Interested\s+Students/);
+    // interested and not otherwise obliged
+    result[fileName]["interested_students"] = [];
+    var interested = [];
+    Object.keys(students).forEach(function (key) {
+        if (key != "Doing Something Else") {
+            console.log(fileName + " " + key + " " + students[key]);
+            students[key].forEach(function (userid) {
+                interested.push(userid);
+            });
+        }
+    });
+    result[fileName]["interested_students"] = interested;
+    // doing something else
+    if (students.hasOwnProperty("Doing Something Else")) {
+        //console.log(fileName + " Doing Something Else: " + students["Doing Something Else"]);
+        result[fileName]["doing_something_else"] = students["Doing Something Else"];
+    } else {
+        result[fileName]["doing_something_else"] = [];
+    }
+
     // Parse Partners
+/*
     result[fileName]["interested_students"] = [];
     // this old code is too fragile --- need a regex
     //var indexIS = fileContent.indexOf("## Interested Students") + 1;
@@ -206,6 +282,7 @@ var parse = function (fileName) {
         }
         indexIS++;
     }
+*/
 
     // Parse Future, Size, Status
     for ( var i = 0; i < fileContent.length; i++){
@@ -221,6 +298,7 @@ var parse = function (fileName) {
     }
 
     // extract metadata
+    //console.log(fileName + " metadata  ------------------------");
     var metadata = extractKeyList(fileContent, 2, /^##\s*Metadata/);
     Object.keys(metadata).forEach(function (key) {
         var value = metadata[key];
@@ -250,7 +328,7 @@ function initParse(){
     for (var i = 0; i < files.length; i++){
         var t = files[i];
         // only parse markdown files; ignore other files in directory
-        if (t.match(/(.*).md$/) && !t.match(/^index/)) {
+        if (t.match(/capco(.*).md$/) && !t.match(/^index/)) {
             res = _.merge(res, parse(t.substring(t.lastIndexOf('/'), t.length)));
         }
     }
