835cab23fb6e2d60180d8b13990d628813788d85
Derek Rayside <drayside@uwaterloo.ca>
2018-03-08 18:24:01 -0500

    embark will go on a different day

--- referee-schedule.sh
+++ referee-schedule.sh
@@ -5,7 +5,9 @@ mkdir -p referees
 
 # generate a script to copy over the referee forms, 
 # and give them the name of the team
+# 2018 Team Embark will present on a different day
 find ../2018/se491/teams -name RefereeForm.md \
+    | grep -v embark \
     | gawk '/md/ {
         split($1, a, "/"); # break up the path
         t=a[5]; # team name is this element
@@ -14,6 +16,7 @@ find ../2018/se491/teams -name RefereeForm.md \
 # run the copying script
 chmod +x .$0.tmp
 ./.$0.tmp
+dos2unix referees/*.md
 
 # start the analysis
 nodejs server.js --referee-schedule-directory ./referees/
