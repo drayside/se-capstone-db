010926469ca73b6f8038c568612046912a3a2c00
Derek Rayside <drayside@uwaterloo.ca>
2018-03-08 15:01:42 -0500

    updating script for 2018

--- referee-schedule.sh
+++ referee-schedule.sh
@@ -3,14 +3,17 @@
 rm -rf referees
 mkdir -p referees
 
-find ../2017/teams -name RefereeForm.md \
+# generate a script to copy over the referee forms, 
+# and give them the name of the team
+find ../2018/se491/teams -name RefereeForm.md \
     | gawk '/md/ {
-        split($1, a, "/"); 
-        t=a[4]; 
+        split($1, a, "/"); # break up the path
+        t=a[5]; # team name is this element
         print "cp", $1, "./referees/" t ".md";}' \
         > .$0.tmp
-
+# run the copying script
 chmod +x .$0.tmp
 ./.$0.tmp
 
+# start the analysis
 nodejs server.js --referee-schedule-directory ./referees/
